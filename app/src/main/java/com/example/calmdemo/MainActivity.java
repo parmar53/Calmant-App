64afbdc3d8b832ed32937c99cf8b4b14ac2d007d
Dhanraj Parmar <dhanrajsinh1998@gmail.com>
2022-05-10 17:44:22 -0400

    Initializing

---
+++ app/src/main/java/com/example/calmdemo/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.calmdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.drawable.Animatable;
+import android.os.Handler;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Animation topAnim,bottomAnim;
+ImageView image;
+TextView logo,slogan;
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //This method is used so that your splash activity
+        //can cover the entire screen.
+
+        setContentView(R.layout.activity_main);
+        //this will bind your MainActivity.class file with activity_main.
+
+        topAnim= AnimationUtils.loadAnimation(this,R.anim.top_animation);
+        bottomAnim= AnimationUtils.loadAnimation(this,R.anim.bottom_animation);
+
+
+        image=findViewById(R.id.imageView2);
+        logo=findViewById(R.id.textView3);
+
+        image.setAnimation(topAnim);
+        logo.setAnimation(bottomAnim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(MainActivity.this,
+                        Login.class);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+    }
+}
